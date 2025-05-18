47815425eb66fa1067c2d68452dfdfe37741c2c6
mdswaley <mdswaley075@gmail.com>
2025-05-18 20:46:00 +0530

    handle global exception

---
+++ inventory-service/src/main/java/com/example/inventoryservice/Advices/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.inventoryservice.Advices;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex){
+        return ResponseEntity.notFound().build();
+    }
+}
