a50c18f845561b1c1faa2a9a7cede2a14bc0a86c
mdswaley <mdswaley075@gmail.com>
2025-05-23 18:56:28 +0530

    Global filter chaining

--- api-gateway/src/main/java/com/example/Api/Gateway/filter/GlobalLoggingFilter.java
+++ api-gateway/src/main/java/com/example/Api/Gateway/filter/GlobalLoggingFilter.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.filter.OrderedFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalLoggingFilter implements GlobalFilter, OrderedFilter {
+public class GlobalLoggingFilter implements GlobalFilter, Ordered {
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Logging from global {}",exchange.getRequest().getURI());
